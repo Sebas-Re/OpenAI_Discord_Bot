34ce134d0afa3594c13a69aee499d7459a395564
Sebas-Re <32151264+Sebas-Re@users.noreply.github.com>
2023-05-08 22:14:41 -0300

    1er push
    Cambios hechos:
    
    Añadido puente con OpenAI, y 3 tipos de comandos:
    
    - /leerchat (Habilita/deshabilita la lectura y comunicacion directa desde el chat, como si el bot fuera un usuario cualquiera
    
    - /consulta (Comando para interactuar con el bot en caso de que /leerchat este en "false"
    
    - /ping (Comprueba el estado del bot)
    
    Cabe notar que /leerchat es una config por servidor, y guarda el estado de la caracteristica en un archivo aparte, para que no se reinicie cada vez que uno tiene que actualizar el bot

---
+++ OpenAI_Discord_Bot.py
@@ -0,0 +1,193 @@
+import os
+import pickle
+import discord
+from discord import app_commands
+import openai
+from discord.ext import commands
+import Secreto
+
+class MyClient(discord.Client):
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+        # A CommandTree is a special type that holds all the application command
+        # state required to make it work. This is a separate class because it
+        # allows all the extra state to be opt-in.
+        # Whenever you want to work with application commands, your tree is used
+        # to store and work with them.
+        # Note: When using commands.Bot instead of discord.Client, the bot will
+        # maintain its own tree instead.
+        self.tree = app_commands.CommandTree(self)
+
+    # In this basic example, we just synchronize the app commands to one guild.
+    # Instead of specifying a guild to every command, we copy over our global commands instead.
+    # By doing so, we don't have to wait up to an hour until they are shown to the end-user.
+    async def setup_hook(self):
+        # This copies the global commands over to your guild.
+        await self.tree.sync()
+
+
+
+bot = MyClient(intents=discord.Intents.all())
+server_settings = {}
+
+
+# Set up the OpenAI API key
+openai.api_key = Secreto.OpenAI_API_KEY
+
+# Set up the Discord bot client
+channels = [258435872020627463, 449413264443441152, 1104014621200883722, 1104014832254070797]
+
+processed_messages = set()
+
+
+# Obtener la ruta del directorio actual
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+# Concatenar el nombre del archivo al final de la ruta
+file_path = os.path.join(dir_path, 'server_settings.pickle')
+
+
+# Funcion para obtener la respuesta de OpenAI a partir de un prompt
+def get_completion(prompt, model="gpt-3.5-turbo"):
+    messages = [{"role": "user", "content": prompt}]
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0, # this is the degree of randomness of the model's output
+        max_tokens=400, # this is the maximum number of tokens that the model will generate
+    )
+    return response.choices[0].message["content"]
+
+
+# Tipos de modelos:
+# gpt-3: el modelo mas grande, pero el mas lento
+# gpt-3.5-turbo: un modelo mas pequeño, pero mas rapido
+# gpt-3.5: un modelo mas pequeño, pero mas rapido
+
+
+
+# Cuando el bot se conecta, cambia su nombre a 'Yggdrasil' y lo informa en consola
+@bot.event
+async def on_ready():
+    await bot.user.edit(username='El Oráculo')
+    print(f'Logged in as {bot.user}!')
+
+    # Carga los server settings, atrapando la excepcion en caso de que no exista el archivo
+    try:
+        with open(file_path, 'rb') as f:
+            global server_settings
+            server_settings= pickle.load(f)
+            f.close()
+    except FileNotFoundError: 
+        server_settings = {}
+        with open(file_path, 'wb') as f:
+            pickle.dump(server_settings, f)
+            f.close()
+
+
+
+
+
+# Cada vez que entra un mensaje al canal seleccionado, el bot lo procesa y responde
+@bot.event
+async def on_message(message):
+
+    # Extrae el id del servidor
+    server_id = message.guild.id
+
+    # Si el servidor esta en la lista de servidores y la caracteristica esta habilitada, procesa el mensaje
+    if server_id in server_settings and server_settings[server_id].get('feature_enabled', False):
+        if message.author == bot.user:
+            return
+        else:
+            username = message.author.name
+            prompt = f"{message.content}"
+            response = get_completion(prompt)
+            print("User ["+username+"] >> "+prompt)
+            print("[OpenAI] >> "+response)
+            await message.reply(response)
+    else:
+        return
+
+
+# Comando para probar que el bot esta vivo
+@bot.tree.command()
+@app_commands.describe()
+async def ping(interaction: discord.Interaction):
+    print('Command received')
+    await interaction.response.send_message(f'Pong! {round(bot.latency * 1000)}ms')
+
+# Comando para habilitar/deshabilitar la caracteristica de lectura de chat
+@bot.tree.command()
+@app_commands.describe()
+async def leerchat(interaction: discord.Interaction):
+    print('Command received')
+    
+    # Extrae el id del servidor
+    server_id = interaction.guild_id
+
+    # Si el servidor no esta en la lista de servidores, lo agrega
+    if server_id not in server_settings:
+        server_settings[server_id] = {}
+
+    # Cambia el estado de la caracteristica
+    server_settings[server_id]['feature_enabled'] = not server_settings[server_id].get('feature_enabled', False)
+
+    # Guarda los cambios en el archivo
+    with open(file_path, 'wb') as f:
+        pickle.dump(server_settings, f)
+        f.close()
+
+    # Informa el estado de la caracteristica
+    if server_settings[server_id]['feature_enabled']:
+        await interaction.response.send_message('Caracteristica habilitada.')
+    else:
+        await interaction.response.send_message('Caracteristica deshabilitada.')
+
+# Comando para consultar a OpenAI 
+@bot.tree.command()
+@app_commands.describe(consulta='Consulta a realizar al bot')
+async def consulta(interaction: discord.Interaction, consulta: str):
+
+    await interaction.response.defer()
+
+    response = get_completion(consulta)
+    print("User Message:"+consulta)
+    print("OpenAI Response:"+response)
+
+    await interaction.edit_original_response(content=response)
+    
+
+bot.run(Secreto.Bot_Token)
+
+
+
+"""
+text-ada-001
+text-embedding-ada-002
+text-similarity-ada-001
+curie-instruct-beta
+ada-code-search-code
+gpt-3.5-turbo-0301
+ada-similarity
+"babbage"
+"davinci"
+"text-davinci-edit-001"
+"babbage-code-search-code"
+"text-similarity-babbage-001"
+"code-davinci-edit-001"
+"text-davinci-001"
+text-search-davinci-query-001
+curie-search-query
+davinci-search-query
+babbage-search-document
+ada-search-document
+text-search-curie-query-001
+curie-similarity
+curie
+text-similarity-davinci-001
+text-davinci-002
+davinci-similarity
+cushman:2020-05-03
+ada:2020-05-03
+"""
